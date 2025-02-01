8a1f0b68a23e49d794476450e4b998c19ca7b98c
BugHunter-X <askzenai@gmail.com>
2025-02-01 23:30:09 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,15 @@
+function printNumber(num: number): void {
+  console.log(num);
+}
+
+function printString(str: string): void {
+  console.log(str);
+}
+
+function printAnything(data: any): void {
+  console.log(data);
+}
+
+printNumber(5);
+printString("hello");
+printAnything(true);
